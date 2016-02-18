ad635e986dedfea443486b536d5a3e39af351411
Miroslav Beka <mirobeka@gmail.com>
2016-02-18 17:49:31 +0100

    Add project files

---
+++ content/02_pycon.md
@@ -0,0 +1,5 @@
+Title: Hello World 2
+Date: 2016-01-02 19:35
+Category: test
+
+second post mother fuckers!
---
+++ pelicanconf.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'sjudeu'
+SITENAME = u'sjudeu.sk'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'))
+
+# Social widget
+SOCIAL = (('github.com', 'http://github.com/mirobeka'),
+          ('facebook.com', 'https://www.facebook.com/miroslavbeka'))
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+# Use Themes
+THEME = 'aboutwilson'
